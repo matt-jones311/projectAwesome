af6dd1df1f3b3e5f9f7d55454e6cf9362a54fda2
Matt Gohr <gohr89@gmail.com>
2018-01-18 20:57:12 -0500

    adding ajax to zomato

--- js/zomato.js
+++ js/zomato.js
@@ -1,27 +1,66 @@
-// This is our API key. Add your own API key between the ""
-var APIKey = "3c4d7a4e578c8bd02aa5aaea088688b1";
+/*
+key =1eccc25c7331c8db8a6403a5c40c24e7
 
-// Here we are building the URL we need to query the database
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&appid=" + APIKey;
+url = https://developers.zomato.com/api/v2.1/locations?query=greek
 
-// We then created an AJAX call
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).done(function(response) {
 
-      // Create CODE HERE to Log the queryURL
-      console.log(queryURL);
-      // Create CODE HERE to log the resulting object
-      console.log(response);
-      // Create CODE HERE to transfer content to HTML
-      $(".city").html("<h1>" + response.name + "</h1>")
-      // Create CODE HERE to calculate the temperature (converted from Kelvin)
-      // Hint: To convert from Kelvin to Fahrenheit: F = (K - 273.15) * 1.80 + 32
-      var temp = response.main.temp;
-      console.log(temp);
-      temp = (temp - 273.15) * 1.80 + 32;
-      console.log(temp);
+param =
 
-      // Create CODE HERE to dump the temperature content into HTML
-      $(".temp").text("Tempature is : " + Math.round(temp));
+
+*/
+
+$(".btn").on("click", function() {
+  var searchTerm = $("#searchterm").val();
+  var num = $("#numberrecords").val();
+  var startDate = $("#startyear").val();
+  var endDate = $("#endyear").val();
+  console.log(searchTerm);
+  console.log(num);
+  console.log(startDate);
+  console.log(endDate);
+
+
+
+  // Built by LucyBot. www.lucybot.com
+  // var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+  var url = "https://developers.zomato.com/api/v2.1/locations?query=greek&user-key=1eccc25c7331c8db8a6403a5c40c24e7";
+
+  // url += '?' + $.param({
+  //   'api-key': "bd4a0a2e3fe049879ea7f7df712460a1",
+  //   'q': searchTerm,
+  //   'page': 0
+  // });
+
+
+  console.log(url);
+  $.ajax({
+    url: url,
+    method: 'GET',
+  }).done(function(result) {
+    console.log(result);
+    console.log('complete');
+
+    for (var i = 0; i < num; i++) {
+      var newRecord = result.response.docs[i];
+      var title = newRecord.headline.main;
+      var author = newRecord.byline.original;
+
+      var newHead = $("<h3>");
+      newHead.text(title);
+      var newName = $("<h4>");
+      newName.text(author);
+
+      $(".output").append(newHead);
+      $(".output").append(newName);
+
+    }
+
+
+  }).fail(function(err) {
+    throw err;
+  })
+
+  //button click
+
+
+})
