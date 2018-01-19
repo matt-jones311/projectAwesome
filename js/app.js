818ce82cea1777807725d498bf4c5a81dba3f42c
Daniel P <danielpimen92@gmail.com>
2018-01-18 20:41:44 -0500

    js file

--- js/app.js
+++ js/app.js
@@ -16,23 +16,3 @@
        	console.log(response);
        	$('#currentTemp').html('The Current Temperature is: ' + response.main.temp );
        });
-
-   /*MARTA API Code*/
-   var APIKey2 = '5f43c2ba-8824-4e1f-aece-ab17d3887188';
-
-   var queryURL2 = 'http://developer.itsmarta.com/RealtimeTrain/RestServiceNextTrain/GetRealtimeArrivals?apikey=' +APIKey2; 
-
-   $.ajax({
-   		url:queryURL2,
-   		method: 'GET'
-
-   })
-   .done(function(response){
-   	console.log(response);
-    var results=response;
-  for (var i = 0; i < results.length; i++) {
-     // console.log(results[i]);
-      console.log(results[1])
-  }
-
-   })
