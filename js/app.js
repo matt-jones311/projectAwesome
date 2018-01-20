48d67b0372918c1fe8e79103a293b8843b745411
Akshay Choudhary <ak92choudhary@gmail.com>
2018-01-20 11:08:38 -0500

    changes to app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
+   
+
+ // Initialize Firebase
+ var config = {
+   apiKey: "AIzaSyDdVcKNzNfmkkcJojS3jYhVm_D8psz3ZGM",
+   authDomain: "project-awesome-fcb21.firebaseapp.com",
+   databaseURL: "https://project-awesome-fcb21.firebaseio.com",
+   projectId: "project-awesome-fcb21",
+   storageBucket: "project-awesome-fcb21.appspot.com",
+   messagingSenderId: "705424388035"
+ };
+ firebase.initializeApp(config);
+
+
+
+
    /*Weather API Code*/
 
    var APIKey = "166a433c57516f51dfab1f7edaed8413";
@@ -27,3 +43,10 @@
        	$('#currentTemp').html('The Current Temperature is: ' + response.main.temp );
        });
 >>>>>>> a2dbf88b8ca306e90583660d909440a76e24ebbb
+
+
+
+
+
+
+
