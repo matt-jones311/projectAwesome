890a5601374c7a2ba6d2a0186b6e613c5419f758
Masen Shreve <masen.shreve@gmail.com>
2018-01-20 11:26:03 -0500

    Firebase

---
+++ firebase.js
@@ -0,0 +1,27 @@
+<script src="https://www.gstatic.com/firebasejs/4.9.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDdVcKNzNfmkkcJojS3jYhVm_D8psz3ZGM",
+    authDomain: "project-awesome-fcb21.firebaseapp.com",
+    databaseURL: "https://project-awesome-fcb21.firebaseio.com",
+    projectId: "project-awesome-fcb21",
+    storageBucket: "project-awesome-fcb21.appspot.com",
+    messagingSenderId: "705424388035"
+  };
+  
+  firebase.initializeApp(config);
+  // VARIABLES
+    // --------------------------------------------------------------------------------
+    var database = firebase.database();
+    var likeCounter = 0;
+    // FUNCTIONS + EVENTS
+    // --------------------------------------------------------------------------------
+$(".fi-like").on("click", function() {
+      likeCounter++;
+      database.ref().set({
+        likeCount: likeCounter
+      });
+    });
+
+</script>
