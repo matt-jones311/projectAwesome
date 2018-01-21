7a4d8466812656d7d00f250df8196abba013d76a
Akshay Choudhary <ak92choudhary@gmail.com>
2018-01-21 17:54:19 -0500

    fixed weather api

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
    
 
-/*Weather API Code*/
+$(document).ready(function(){
 
-   /*var APIKey = "166a433c57516f51dfab1f7edaed8413";
+   var APIKey = "166a433c57516f51dfab1f7edaed8413";
 
    // Here we are building the URL we need to query the database
    var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
@@ -10,7 +10,7 @@
 
    // Here we run our AJAX call to the OpenWeatherMap API
    $.ajax({
-<<<<<<< HEAD
+
        url: queryURL,
        method: "GET"
      })
@@ -19,18 +19,9 @@
        console.log(response);
        $('#currentTemp').html('The Current Temperature is: ' + response.main.temp);
      });
-=======
-           url: queryURL,
-           method: "GET"
-       })
-       // We store all of the retrieved data inside of an object called "response"
-       .done(function(response) {
-        console.log(response);
-        $('#currentTemp').html('The Current Temperature is: ' + response.main.temp );
-       });
->>>>>>> a2dbf88b8ca306e90583660d909440a76e24ebbb */ 
 
-$(document).ready(function(){
+      
+
 
  // Initialize Firebase
  var config = {
