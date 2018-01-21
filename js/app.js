19489978e7b815f1741f3b707fafa524f24f62aa
Akshay Choudhary <ak92choudhary@gmail.com>
2018-01-21 16:53:27 -0500

    changes in restaurant.html and app.js

--- js/app.js
+++ js/app.js
@@ -1,22 +1,8 @@
    
 
- // Initialize Firebase
- var config = {
-   apiKey: "AIzaSyDdVcKNzNfmkkcJojS3jYhVm_D8psz3ZGM",
-   authDomain: "project-awesome-fcb21.firebaseapp.com",
-   databaseURL: "https://project-awesome-fcb21.firebaseio.com",
-   projectId: "project-awesome-fcb21",
-   storageBucket: "project-awesome-fcb21.appspot.com",
-   messagingSenderId: "705424388035"
- };
- firebase.initializeApp(config);
+/*Weather API Code*/
 
-
-
-
-   /*Weather API Code*/
-
-   var APIKey = "166a433c57516f51dfab1f7edaed8413";
+   /*var APIKey = "166a433c57516f51dfab1f7edaed8413";
 
    // Here we are building the URL we need to query the database
    var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
@@ -39,13 +25,89 @@
        })
        // We store all of the retrieved data inside of an object called "response"
        .done(function(response) {
-       	console.log(response);
-       	$('#currentTemp').html('The Current Temperature is: ' + response.main.temp );
+        console.log(response);
+        $('#currentTemp').html('The Current Temperature is: ' + response.main.temp );
        });
->>>>>>> a2dbf88b8ca306e90583660d909440a76e24ebbb
+>>>>>>> a2dbf88b8ca306e90583660d909440a76e24ebbb */ 
+
+$(document).ready(function(){
+
+ // Initialize Firebase
+ var config = {
+   apiKey: "AIzaSyDdVcKNzNfmkkcJojS3jYhVm_D8psz3ZGM",
+   authDomain: "project-awesome-fcb21.firebaseapp.com",
+   databaseURL: "https://project-awesome-fcb21.firebaseio.com",
+   projectId: "project-awesome-fcb21",
+   storageBucket: "project-awesome-fcb21.appspot.com",
+   messagingSenderId: "705424388035"
+ };
+ firebase.initializeApp(config);
 
 
 
+
+   
+
+var foodData = firebase.database();
+  // 2. Button for adding Restaurants
+  $("#addRestaurantBtn").on("click", function(){
+
+    // Grabs user input and assign to variables
+    var restaurantName = $("#restaurantInput").val().trim();
+    var foodName = $("#foodInput").val().trim();
+    var priceInput = $("#priceInput").val().trim();
+    
+    var ratingInput = $("#ratingInput").val().trim();
+
+    // Test for variables entered
+    console.log(restaurantName);
+    console.log(foodName);
+    console.log(priceInput);
+   
+    console.log(ratingInput);
+
+    // Creates local "temporary" object for holding train data
+    // Will push this to firebase
+    var newRestaurant = {
+      name:  restaurantName,
+      food: foodName,
+      price: priceInput,
+      rating: ratingInput,
+      
+    }
+
+    // pushing trainInfo to Firebase
+    foodData.ref().push(newRestaurant);
+    
+
+    // clear text-boxes
+    $("#restaurantInput").val("");
+    $("#foodInput").val("");
+    $("#priceInput").val("");
+    $("#ratingInput").val("");
+    
+
+    // Prevents page from refreshing
+    return false;
+  });
+
+  foodData.ref().on("child_added", function(childSnapshot, prevChildKey){
+
+    console.log(childSnapshot.val());
+
+    // assign firebase variables to snapshots.
+    var firebaseName = childSnapshot.val().name;
+    var firebaseFood = childSnapshot.val().food;
+    var firebasePrice = childSnapshot.val().price;
+    var firebaseRating = childSnapshot.val().rating;
+    
+    
+    
+    // Append train info to table on page
+    $("#foodTable > tbody").append("<tr><td>" + firebaseName + "</td><td>" + firebaseFood + "</td><td>"+ firebasePrice + "</td><td>" + firebaseRating + "</td></tr>");
+
+  });
+});
 
 
 
